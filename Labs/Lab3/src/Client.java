07082753d4c9ac3e58d9539e211563c758623851
Martim de Carvalho e Sousa Pinto da Silva <martimpintodasilva@gmail.com>
2020-03-05 13:54:00 +0000

    lab3 done

--- Labs/Lab3/src/Client.java
+++ Labs/Lab3/src/Client.java
@@ -1,13 +1,10 @@
-import java.rmi.NotBoundException;
-import java.rmi.RemoteException;
-
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
 /*
-    java Client <host> <port> <oper> <opnd>*
-        <host> : is the DNS name (or the IP address, in the dotted decimal format) where the server is running
-        <port> : port number the server is using to provide the service
+    java Client <host_name> <remote_object_name> <oper> <opnd>*
+        <host_name> : is the DNS name (or the IP address, in the dotted decimal format) where the server is running
+        <remote_object_name> : The name to which the remote object shall be bound
         <oper> : can be either "register" or "lookup"
         <opnd>* : for "register" -> <DNS name> <IP address>
                 : for "lookup"   -> <DNS name>
@@ -15,55 +12,32 @@ import java.rmi.registry.Registry;
 
 public class Client {
 
-    public static void main(String[] args) throws RemoteException, NotBoundException {
-        if(!(args[2].equals("register") && args.length == 5) && !(args[2].equals("lookup") && args.length == 4)) {
-            System.out.println("Usage: java Client <host> <port> <oper> <opnd>*");
+    public static void main(String[] args) {
+        if(!(args[2].equals("register") && args.length == 4) && !(args[2].equals("lookup") && args.length == 4)) {
+            System.out.println("Usage: java Client <host_name> <remote_object_name> <oper> <opnd>*");
         }
 
-        String host_name = args[0];
-        String object_name = args[1];
-        String operation = args[2];
-        String dns_name = args[3];
-        // Client client = new Client(args[0], args[1], args[2], args[3]);
+        try {
+            String response;
+            Registry registry = LocateRegistry.getRegistry(args[0]);
+            ServerInterface serverStub = (ServerInterface) registry.lookup(args[1]);
 
+            if(args[2].equals("register")) {
+                response = serverStub.register(args[3], args[4]);
+            }
+            else if(args[2].equals("lookup")) {
+                response = serverStub.lookup(args[3]);
+            }
+            else {
+                System.err.println("Usage: java Client <host_name> <remote_object_name> <oper> <opnd>");
+                return;
+            }
 
-        Registry registry = LocateRegistry.getRegistry(null);
-        ServerInterface serverStub = (ServerInterface) registry.lookup(object_name);
-        //ServerInterface serverStub = (ServerInterface) registry.register(object_name);
-        System.out.println(serverStub.lookup(dns_name));
+            System.out.println("response: " + response);
+
+        } catch (Exception e) {
+            System.err.println("Client exception: " + e.toString());
+            e.printStackTrace();
+        }
     }
-
-    /*public Client(String hostName, String objectName, String operation, String dnsName) throws IOException {
-        public Client(String hostName, String objectName, String operation, String dnsName) throws IOException {
-            this.hostName = hostName;
-            this.objectName = objectName;
-            this.operation = operation;
-            this.dnsName = dnsName;
-        }
-    }*/
 }
-
-
-
-
-
-/*
-*  usar a classe naming ou o outro metodo static
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-* */
--- Labs/Lab3/src/Server.java
+++ Labs/Lab3/src/Server.java
@@ -1,35 +1,45 @@
 import java.io.*;
 import java.net.*;
 import java.rmi.AlreadyBoundException;
+import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.HashMap;
 
 /*
-    java Server <port number>
-        <port number> : port number the server shall use to provide the service
+    java Server <remote_object_name>
+        <remote_object_name> : The name to which the remote object shall be bound
 */
 
 public class Server implements ServerInterface{
-    public HashMap<String, String> dnsIp;
+    private HashMap<String, String> dnsIp;
 
-    public static void main(String args[]) throws IOException {
+    public static void main(String args[]) {
         if (args.length != 1) {
-            System.out.println("Usage: java Server <port number>");
+            System.out.println("Usage: java Server <remote_object_name>");
             return;
         }
 
+        // Receives the object name in the command line
+        String objectName = args[0];
         // Programmatically set the value of the property java.rmi.server.codebase to the location of the codebase
         System.setProperty("java.rmi.server.codebase", "file:///C://Users/Martim/Desktop/MIEIC_sdis/Labs/Lab3/out/production/RMI/");
-        // instantiate the "remote object".
-        Server obj = new Server();
-        // "export" the remote object and produce the respective stub
-        ServerInterface stub = (ServerInterface) UnicastRemoteObject.exportObject(obj, 0);
-        // Register the stub (returns an object that represents the rmi registry)
-        Registry registry = LocateRegistry.getRegistry(2001);
-        // It is preferable to use rebind(…), as bind(…) will throw an exception if the previously registered name is reused
-        registry.rebind(args[0], stub);
+
+        try {
+            // Instantiate the "remote object".
+            Server obj = new Server();
+            // "Export" the remote object and produce the respective stub
+            ServerInterface stub = (ServerInterface) UnicastRemoteObject.exportObject(obj, 0);
+            // Register the stub (returns an object that represents the rmi registry)
+            Registry registry = LocateRegistry.getRegistry(1099);
+            // It is preferable to use rebind(…), as bind(…) will throw an exception if the previously registered name is reused
+            registry.rebind(objectName, stub);
+            System.err.println("Server ready");
+        } catch (Exception e) {
+            System.err.println("Server exception: " + e.toString());
+            e.printStackTrace();
+        }
     }
 
     public Server(){
