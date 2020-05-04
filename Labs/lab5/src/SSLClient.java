353e6c5425176d85c18cbf87e73f3e64436fb592
Martim de Carvalho e Sousa Pinto da Silva <martimpintodasilva@gmail.com>
2020-05-04 12:21:27 +0100

    lab5 done

--- Labs/lab5/src/SSLClient.java
+++ Labs/lab5/src/SSLClient.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.net.*;
 
 /*
-    java Client <host> <port> <oper> <opnd>*
+    java Client <host> <port> <oper> <opnd>* <cypher suites>*
         <host> : is the DNS name (or the IP address, in the dotted decimal format) where the server is running
         <port> : port number the server is using to provide the service
         <oper> : can be either "register" or "lookup"
@@ -22,73 +22,73 @@ public class SSLClient {
     private String[] cipherSuits;
 
     public static void main(String[] args) throws IOException {
-        if(!(args[2].equals("register") && args.length == 5) && !(args[2].equals("lookup") && args.length == 4)) {
-            System.out.println("Usage: java Client <host> <port> <oper> <opnd>*");
+        if(!(args[2].equals("register") && args.length >= 5) && !(args[2].equals("lookup") && args.length >= 4)) {
+            System.out.println("Usage: java Client <host> <port> <oper> <opnd>* <cypher suites>*");
         }
 
-        SSLClient client = new SSLClient(args[0], Integer.parseInt(args[1]), args);
-
-        // send packet
+        SSLClient client = new SSLClient(args);
         client.send(client.build(args));
-
-        // receive packet and print result
         client.print(client.receive(), args);
-
-        // close socket
         client.destroy();
     }
 
-    public SSLClient(String address, int port, String[] args) throws IOException {
-        this.port = port;
+    public SSLClient(String[] args) throws IOException {
+        this.port = Integer.parseInt(args[1]);
         this.address = InetAddress.getLocalHost();
-        this.sslSocketFactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
-        this.sslSocket = (SSLSocket) this.sslSocketFactory.createSocket(this.address, port);
 
-        /*System.setProperty("javax.net.ssl.trustStore", "truststore");
-        System.setProperty("javax.net.ssl.trustStoreType","JKS");
-        System.setProperty("javax.net.ssl.keyStore", "client.keys");*/
-
-        this.sslSocket.setEnabledProtocols(this.sslSocket.getSupportedProtocols());
-
-        for(int i = 2; i < args.length; i++){
-            this.cipherSuits[i-1] = args[i];
+        int operationLength;
+        if(operationIsRegister(args)) {
+            operationLength = 5;
+        } else {
+            operationLength = 4;
         }
+
+        this.cipherSuits = new String[args.length - operationLength];
+        for(int i = operationLength; i < args.length; i++)
+            this.cipherSuits[i-operationLength] = args[i];
+
+        this.sslSocketFactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+        this.sslSocket = (SSLSocket) this.sslSocketFactory.createSocket(this.address, this.port);
+        this.sslSocket.setEnabledProtocols(this.sslSocket.getSupportedProtocols());
         this.sslSocket.setEnabledCipherSuites(this.cipherSuits);
+
+        this.sslSocket.startHandshake();
 
         this.input = new BufferedReader(new InputStreamReader(this.sslSocket.getInputStream()));
         this.output = new PrintWriter(this.sslSocket.getOutputStream(), true);
     }
 
-    private void send(String message) throws IOException {
+    private void send(String message) {
         output.println(message);
     }
 
     private String receive() throws IOException {
-        // receive response
-        String response = input.readLine().trim();
-        return response;
+        return input.readLine().trim();
     }
 
     private String build(String[] args) {
         String message = args[2];
 
-        for (int i = 3; i < args.length; i++) {
+        for (int i = 3; i < args.length; i++)
             message += " " + args[i];
-        }
 
         return message;
     }
 
     private void print(String result, String[] args) {
         if(!result.equals("-1")) {
-            System.out.println("Client: " + this.build(args) + " : " + result);
+            System.out.println("Client: " + this.build(args) + ": " + result);
         } else {
-            System.out.println("Client: " + this.build(args) + " : ERROR");
+            System.out.println("Client: " + this.build(args) + ": ERROR");
         }
     }
 
-    public void destroy() throws IOException {
+    private void destroy() throws IOException {
         this.sslSocket.shutdownOutput();
         this.sslSocket.close();
+    }
+
+    private boolean operationIsRegister(String[] args) {
+        return args[2].equals("register");
     }
 }
--- Labs/lab5/src/SSLServer.java
+++ Labs/lab5/src/SSLServer.java
@@ -5,69 +5,50 @@ import java.io.*;
 import java.net.*;
 import java.util.HashMap;
 
-/*
-    java Server <port number>
-        <port number> : port number the server shall use to provide the service
-*/
-
 public class SSLServer {
     public HashMap<String, String> dnsIp;
+    public int port;
     private SSLServerSocket socket = null;
     private SSLServerSocketFactory socketFactory = null;
-    public int port;
     private String[] cipherSuits;
 
     public static void main(String args[]) throws IOException {
-        if (args.length != 1) {
-            System.out.println("Usage: java Server <port number>");
+        if (args.length < 2) {
+            System.out.println("Usage: java Server <port number> <cipher suites>*");
             return;
         }
 
-        SSLServer server = new SSLServer(Integer.parseInt(args[0]), args);
+        SSLServer server = new SSLServer(args);
 
-        while(true) {
-            server.run();
-        }
-
-        // server.serverSocketClose();
+        while(true) server.run();
     }
 
-    public SSLServer(int port, String[] args) throws IOException {
+    public SSLServer(String[] args) throws IOException {
+        this.port = Integer.parseInt(args[0]);
+        this.dnsIp = new HashMap<>();
 
-        this.port = port;
-        this.dnsIp = new HashMap<String, String>();
+        this.cipherSuits = new String[args.length - 1];
+        for(int i = 1; i < args.length; i++)
+            this.cipherSuits[i-1] = args[i];
 
         this.socketFactory = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
         this.socket = (SSLServerSocket) this.socketFactory.createServerSocket(port);
-
         this.socket.setNeedClientAuth(true);
         this.socket.setEnabledProtocols(this.socket.getSupportedProtocols());
-
-        for(int i = 1; i < args.length; i++){
-            this.cipherSuits[i-1] = args[i];
-        }
         this.socket.setEnabledCipherSuites(this.cipherSuits);
-
-        /*System.setProperty("javax.net.ssl.trustStore", "truststore");
-        System.setProperty("javax.net.ssl.trustStoreType","JKS");
-        System.setProperty("javax.net.ssl.keyStore", "server.keys");*/
     }
 
     private void run() throws IOException {
-
         Socket dataSocket  = this.socket.accept();
-
         BufferedReader input = new BufferedReader(new InputStreamReader(dataSocket.getInputStream()));
 
-        // parse request
         String request = input.readLine();
         request.trim();
 
         PrintWriter output = new PrintWriter(dataSocket.getOutputStream(), true);
         String reply = this.process(request);
 
-        System.out.println(request + "::" + reply);
-
+        System.out.println("Server: " + request + ": " + reply);
         output.println(reply);
 
         this.dataSocketClose(dataSocket);
@@ -75,36 +56,28 @@ public class SSLServer {
 
     private String process(String request) {
         String[] tokens = request.trim().split(" ");
-        String reply;
 
         if(tokens[0].equals("register")) {
             if(!this.dnsIp.containsKey(tokens[1])) {
                 this.dnsIp.put(tokens[1], tokens[2]);
-                return this.dnsIp.size() + "\n" + tokens[1] + " " + tokens[2];
+                return tokens[1] + " " + tokens[2];
             } else {
-                return -1 + "\n" + tokens[1] + " " + tokens[2];
+                return "-1";
             }
         } else if(tokens[0].equals("lookup")) {
-            String name;
             if(!this.dnsIp.containsKey(tokens[1])) {
-                return -1 + "\n" + tokens[1] + " " + dnsIp.get(tokens[1]);
+                return "-1";
             } else {
-                return this.dnsIp.size() + "\n" + tokens[1] + " " + dnsIp.get(tokens[1]);
+                return tokens[1] + " " + dnsIp.get(tokens[1]);
             }
         } else {
             System.out.println("Error parsing message");
             return "-1";
         }
-
-
     }
 
     public void dataSocketClose(Socket socket) throws IOException {
         socket.shutdownOutput();
         socket.close();
-    }
-
-    public void serverSocketClose() throws IOException {
-        this.socket.close();
     }
 }
